0bae5339a03176f93c299809c82f4931571a7014
Brian Anderson <banderson@mozilla.com>
2016-05-11 01:03:43 +0000

    Get the mir from the AST

--- src/trans.rs
+++ src/trans.rs
@@ -11,19 +11,26 @@ pub fn translate_crate<'tcx>(tcx: &TyCtxt<'tcx>,
                              mir_map: &MirMap<'tcx>) -> Result<()> {
     let _ignore = tcx.dep_graph.in_ignore();
 
-    let ref mut v = HirVisitor;
+    let ref mut v = HirVisitor { mir_map: mir_map };
 
     tcx.map.krate().visit_all_items(v);
 
     Ok(())
 }
 
-struct HirVisitor;
+struct HirVisitor<'a, 'tcx: 'a> {
+    mir_map: &'a MirMap<'tcx>
+}
 
-impl<'v> Visitor<'v> for HirVisitor {
+impl<'v, 'a: 'tcx, 'tcx> Visitor<'v> for HirVisitor<'a, 'tcx> {
     fn visit_fn(&mut self, fk: FnKind<'v>, fd: &'v FnDecl,
-                b: &'v Block, s: Span, _: NodeId) {
-        debug!("{:?}", fd);
+                b: &'v Block, s: Span, id: NodeId) {
+        debug!("visiting fn {:?}", fd);
+
+        let mir = self.mir_map.map.get(&id);
+
+        //debug!("fn mir: {:?}", mir);
+
         intravisit::walk_fn(self, fk, fd, b, s)
     }
 }
