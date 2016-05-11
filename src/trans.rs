c52ab62221c19c842224ab1191fcf1fb247fe75e
Alon Zakai <alonzakai@gmail.com>
2016-05-11 12:00:47 -0700

    create just one module, put all functions on it, and dispose of it properly

--- src/trans.rs
+++ src/trans.rs
@@ -13,25 +13,33 @@ use binaryen::*;
 pub fn translate_crate<'tcx>(tcx: &TyCtxt<'tcx>,
                              mir_map: &MirMap<'tcx>) -> Result<()> {
 
+    let module: BinaryenModuleRef;
+
+    unsafe {
+        module = BinaryenModuleCreate();
+    }
+
     for (&id, mir) in &mir_map.map {
         for attr in tcx.map.attrs(id) {
             let item = tcx.map.expect_item(id);
-            println!("Processing function: {}", item.name);
+
+            println!("Processing function ({}): {}", id, item.name);
 
             unsafe {
-                let module = BinaryenModuleCreate();
-
                 let param = BinaryenInt32();
                 let ii = BinaryenAddFunctionType(module, CString::new("iii").unwrap().as_ptr(), BinaryenInt32(), &param, 1);
 
                 let add = BinaryenBinary(module, BinaryenAdd(), BinaryenGetLocal(module, 0, BinaryenInt32()), BinaryenGetLocal(module, 0, BinaryenInt32()));
 
-                let adder = BinaryenAddFunction(module, CString::new(item.name.as_str().as_bytes()).unwrap().as_ptr(), ii, ptr::null(), 0, add);
-
-                BinaryenModulePrint(module);
+                BinaryenAddFunction(module, CString::new(item.name.as_str().as_bytes()).unwrap().as_ptr(), ii, ptr::null(), 0, add);
             }
 
         }
+    }
+
+    unsafe {
+        BinaryenModulePrint(module);
+        BinaryenModuleDispose(module);
     }
 
     Ok(())
