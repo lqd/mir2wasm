5978589cfdd77e4545f2f1d7750233c73ac827d2
Brian Anderson <banderson@mozilla.com>
2016-05-12 05:39:11 +0000

    wip

--- src/trans.rs
+++ src/trans.rs
@@ -11,23 +11,32 @@ pub fn translate_crate<'tcx>(tcx: &TyCtxt<'tcx>,
                              mir_map: &MirMap<'tcx>) -> Result<()> {
     let _ignore = tcx.dep_graph.in_ignore();
 
-    let ref mut v = HirVisitor { mir_map: mir_map };
+    let ref mut v = HirVisitor {
+        tcx: tcx,
+        mir_map: mir_map
+    };
 
     tcx.map.krate().visit_all_items(v);
 
     Ok(())
 }
 
-struct HirVisitor<'a, 'tcx: 'a> {
-    mir_map: &'a MirMap<'tcx>
+struct HirVisitor<'v, 'tcx: 'v> {
+    tcx: &'v TyCtxt<'tcx>,
+    mir_map: &'v MirMap<'tcx>
 }
 
-impl<'v, 'a: 'tcx, 'tcx> Visitor<'v> for HirVisitor<'a, 'tcx> {
+impl<'v, 'tcx> Visitor<'v> for HirVisitor<'v, 'tcx> {
     fn visit_fn(&mut self, fk: FnKind<'v>, fd: &'v FnDecl,
                 b: &'v Block, s: Span, id: NodeId) {
         debug!("visiting fn {:?}", fd);
 
         let mir = self.mir_map.map.get(&id);
+        //let did = self.cx.map.local_def_id(&id);
+        //let ty = self.cx.lookup_item_type(did);
+        // look up def_id lookup_item_type
+        // get type with fn_sig/fn_args/fn_ret
+
 
         //debug!("fn mir: {:?}", mir);
 
