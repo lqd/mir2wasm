fd3d965a7bb97695a80221c3479aea02c9470ba5
Eric Holk <eholk@google.com>
2016-06-30 15:49:57 -0700

    Refactor tests and add a compile_pass suite.

---
+++ tests/compile-pass/trivial.rs
@@ -0,0 +1,12 @@
+#![allow(dead_code)]
+#![no_std]
+#![no_core]
+
+fn empty() {}
+
+fn unit_var() {
+    let x = ();
+    x
+}
+
+fn main() {}
--- tests/compiletest.rs
+++ tests/compiletest.rs
@@ -3,7 +3,9 @@ extern crate compiletest_rs as compiletest;
 use std::path::{PathBuf, Path};
 use std::io::Write;
 
-fn compile_fail(sysroot: &str) {
+#[test] #[ignore]
+fn compile_fail() {
+    let sysroot = &find_sysroot();
     let flags = format!("--sysroot {} -Dwarnings", sysroot);
     for_all_targets(sysroot, |target| {
         let mut config = compiletest::default_config();
@@ -18,6 +20,48 @@ fn compile_fail(sysroot: &str) {
     });
 }
 
+#[test]
+fn compile_pass() {
+    let sysroot = find_sysroot();
+    for_all_targets(&sysroot, |target| {
+        for file in std::fs::read_dir("tests/compile-pass").unwrap() {
+            let file = file.unwrap();
+            let path = file.path();
+
+            if !file.metadata().unwrap().is_file() || !path.to_str().unwrap().ends_with(".rs") {
+                continue;
+            }
+
+            let stderr = std::io::stderr();
+            write!(stderr.lock(), "test [compile-pass] {} ... ", path.display()).unwrap();
+            let mut cmd = std::process::Command::new("target/debug/mir2wasm");
+            cmd.arg(path);
+            cmd.arg("-Dwarnings");
+            let libs = Path::new(&sysroot).join("lib");
+            let sysroot = libs.join("rustlib").join(&target).join("lib");
+            let paths = std::env::join_paths(&[libs, sysroot]).unwrap();
+            cmd.env(compiletest::procsrv::dylib_env_var(), paths);
+
+            match cmd.output() {
+                Ok(ref output) if output.status.success() => writeln!(stderr.lock(), "ok").unwrap(),
+                Ok(output) => {
+                    writeln!(stderr.lock(), "FAILED with exit code {:?}", output.status.code()).unwrap();
+                    writeln!(stderr.lock(), "stdout: \n {}", std::str::from_utf8(&output.stdout).unwrap()).unwrap();
+                    writeln!(stderr.lock(), "stderr: \n {}", std::str::from_utf8(&output.stderr).unwrap()).unwrap();
+                    panic!("some tests failed");
+                }
+                Err(e) => {
+                    writeln!(stderr.lock(), "FAILED: {}", e).unwrap();
+                    panic!("some tests failed");
+                },
+            }
+        }
+        let stderr = std::io::stderr();
+        writeln!(stderr.lock(), "").unwrap();
+    });
+}
+
+#[test]
 fn run_pass() {
     let mut config = compiletest::default_config();
     config.mode = "run-pass".parse().expect("Invalid mode");
@@ -25,41 +69,9 @@ fn run_pass() {
     compiletest::run_tests(&config);
 }
 
-fn for_all_targets<F: FnMut(String)>(sysroot: &str, mut f: F) {
-    for target in std::fs::read_dir(format!("{}/lib/rustlib/", sysroot)).unwrap() {
-        let target = target.unwrap();
-        if !target.metadata().unwrap().is_dir() {
-            continue;
-        }
-        let target = target.file_name().into_string().unwrap();
-        if target == "etc" {
-            continue;
-        }
-        let stderr = std::io::stderr();
-        writeln!(stderr.lock(), "running tests for target {}", target).unwrap();
-        f(target);
-    }
-}
-
-#[test]
-fn empty_test() {
-    // show the test harness is running by getting at least one
-    // successful test.
-}
-
 #[test] #[ignore]
-fn compile_test() {
-    // Taken from https://github.com/Manishearth/rust-clippy/pull/911.
-    let home = option_env!("RUSTUP_HOME").or(option_env!("MULTIRUST_HOME"));
-    let toolchain = option_env!("RUSTUP_TOOLCHAIN").or(option_env!("MULTIRUST_TOOLCHAIN"));
-    let sysroot = match (home, toolchain) {
-        (Some(home), Some(toolchain)) => format!("{}/toolchains/{}", home, toolchain),
-        _ => option_env!("RUST_SYSROOT")
-            .expect("need to specify RUST_SYSROOT env var or use rustup or multirust")
-            .to_owned(),
-    };
-    compile_fail(&sysroot);
-    run_pass();
+fn miri_run_pass() {
+    let sysroot = find_sysroot();
     for_all_targets(&sysroot, |target| {
         for file in std::fs::read_dir("tests/run-pass").unwrap() {
             let file = file.unwrap();
@@ -71,7 +83,7 @@ fn compile_test() {
 
             let stderr = std::io::stderr();
             write!(stderr.lock(), "test [miri-pass] {} ... ", path.display()).unwrap();
-            let mut cmd = std::process::Command::new("target/debug/miri");
+            let mut cmd = std::process::Command::new("target/debug/mir2wasm");
             cmd.arg(path);
             cmd.arg("-Dwarnings");
             cmd.arg(format!("--target={}", target));
@@ -97,4 +109,38 @@ fn compile_test() {
         let stderr = std::io::stderr();
         writeln!(stderr.lock(), "").unwrap();
     });
+}
+
+fn for_all_targets<F: FnMut(String)>(sysroot: &str, mut f: F) {
+    for target in std::fs::read_dir(format!("{}/lib/rustlib/", sysroot)).unwrap() {
+        let target = target.unwrap();
+        if !target.metadata().unwrap().is_dir() {
+            continue;
+        }
+        let target = target.file_name().into_string().unwrap();
+        if target == "etc" {
+            continue;
+        }
+        let stderr = std::io::stderr();
+        writeln!(stderr.lock(), "running tests for target {}", target).unwrap();
+        f(target);
+    }
+}
+
+#[test]
+fn empty_test() {
+    // show the test harness is running by getting at least one
+    // successful test.
+}
+
+fn find_sysroot() -> String {
+    // Taken from https://github.com/Manishearth/rust-clippy/pull/911.
+    let home = option_env!("RUSTUP_HOME").or(option_env!("MULTIRUST_HOME"));
+    let toolchain = option_env!("RUSTUP_TOOLCHAIN").or(option_env!("MULTIRUST_TOOLCHAIN"));
+    match (home, toolchain) {
+        (Some(home), Some(toolchain)) => format!("{}/toolchains/{}", home, toolchain),
+        _ => option_env!("RUST_SYSROOT")
+            .expect("need to specify RUST_SYSROOT env var or use rustup or multirust")
+            .to_owned(),
+    }
 }
